c2715c3107960644edef34557159f7e879406eed
Liam Howell <liammm@gmail.com>
2024-12-21 21:31:19 -0500

    chore: update entrypoint script to conditionally seed database based on NODE_ENV

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,13 @@ set -e
 echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
-echo "Seeding the database..."
-npx prisma db seed
+
+if [ "$NODE_ENV" != "production" ]; then
+  echo "Seeding the database..."
+  npx prisma db seed
+else
+  echo "Skipping seeding because NODE_ENV=$NODE_ENV"
+fi
 
 echo "Starting the server..."
 exec "$@"
